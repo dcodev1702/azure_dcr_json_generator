be53c4f2c1d0100d67c9d280826c32d95701c16d
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-02-18 17:32:12 -0500

    Added the ability to use a command line arugment of 0 or 1 to delete existing *.json

--- generate_json_for_dcr_docker.sh
+++ generate_json_for_dcr_docker.sh
@@ -5,7 +5,6 @@
 # Author: dcodev1702
 # Date: 02.18.2023
 # Purpose: Basic shell script to:
-#  -- Create tmp directory in the present working directory (pwd)
 #  -- Map STD|JSON logstash pipeline conf to /usr/share/logstash/pipeline/
 #  -- Map config files used by Logstash
 #  -- Run logstash command to convert unstructured log entry to JSON 
@@ -19,17 +18,28 @@ logstash_conf="/usr/share/logstash/config"
 logstash_json_conf="stdin-dcr-sentinel-sample.conf"
 logstash_pipeline="/usr/share/logstash/pipeline"
 apache2Log="apache2_accesslog_entry.txt"
-container="logstash_azure_dcr"  # docker image ls
-RM_JSON_FLAG='0'
+container="temp_logstash"
 
-# Create /tmp directory for sample JSON file (output)
-echo -e "\e[32m\nCreating tmp directory where the sample JSON file will reside\e[0m\n"
+
+if [ "$#" -eq 0 ]; then
+   RM_JSON_FLAG=0
+else
+   RM_JSON_FLAG=1
+fi
+
+# Create ./tmp directory for sample JSON file (output)
+echo -e "\e[32m\nCreating tmp directory where the sample JSON file will reside\e[0m"
 if [ -d "$(pwd)/tmp" ]; then
-   echo -e "tmp directory already exists! Removing old JSON sample files...\n"
+   echo -e "$(pwd)/tmp directory already exists | RM_JSON_FLAG = $RM_JSON_FLAG \n"
+
+   if [ $RM_JSON_FLAG -eq 1 ]; then
+      sudo rm -rfv ./tmp/*.json
+   fi
 else
    mkdir -p -v "$(pwd)/tmp"
    echo -e "\033[33m$(pwd)/tmp directory successfully created..\033[0m\n"
 fi
+
 
 echo -e "\e[32m\nGenerating Apache2 Log entry in JSON format for Data Collection Rule Transformation\e[0m"
 
@@ -39,7 +49,7 @@ cat ./$apache2Log | docker run -i --rm --cpus="2.0" \
   -v "$(pwd)/tmp:$logstash_dir" \
   -v "$(pwd)/logstash.yml:$logstash_conf/logstash.yml" \
   -v "$(pwd)/$logstash_json_conf:$logstash_pipeline/$logstash_json_conf" \
-  $container -f "$logstash_pipeline/$logstash_json_conf"
+  $container -f "$logstash_pipeline/$logstash_json_conf" > /dev/null 2>&1
 
 echo -e "\033[33mGo to ./tmp and your sample JSON file will be there.\033[0m"
 echo -e "\033[33mUpload this JSON file to DCR Transformation Editor in order to normalize the apache2 log entry for Log Analytics.\033[0m\n"
